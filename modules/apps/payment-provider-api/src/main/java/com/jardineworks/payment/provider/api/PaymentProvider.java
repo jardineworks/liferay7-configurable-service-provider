25003eaebd8374cc7ac4c6ef34d13709c602bdeb
jardineworks <Hutm2ic!0>
2020-01-07 21:08:17 -0500

    added payment provider interface for provider contracts

---
+++ modules/apps/payment-provider-api/src/main/java/com/jardineworks/payment/provider/api/PaymentProvider.java
@@ -0,0 +1,14 @@
+package com.jardineworks.payment.provider.api;
+
+/**
+ * PaymentProvider.java
+ * <p>
+ *  Interface class that is used to define the contract that all payment provider service implementations 
+ *  must adhere to.
+ * </p>s
+ * 
+ * @author andrew jardine | jardine works inc.
+ */
+public interface PaymentProvider {
+
+}
